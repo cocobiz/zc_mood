5fb1bdca6dbf33cbe6bcdbf6c76277c87bfa20f4
fangfei-home <cocobiz@126.com>
2015-03-07 16:25:07 +0800

    ss

---
+++ includes/templates/mood/templates/tpl_advanced_search_result_default.php
@@ -0,0 +1,84 @@
+<div id="center">
+    <div class="inner-container">
+        <div class="product-listing-wrapper">
+            <div class="split-page fix">
+                <ul class="limit fix l">
+                    <li style="padding-right: 5px" class="l">Items Per Page:</li>
+                    <li class="l">
+                        <a href="http://localhost/zc_mood/index.php?main_page=index&amp;cPath=65&amp;limit=27" class="">27</a>
+                    </li>
+                    <li class="l" style="padding: 0 5px">|</li>
+                    <li class="l">
+                        <a href="http://localhost/zc_mood/index.php?main_page=index&amp;cPath=65&amp;limit=54" class="">54</a>
+                    </li>
+                    <li class="l" style="padding: 0 5px">|</li>
+                    <li class="l">
+                        <a href="http://localhost/zc_mood/index.php?main_page=index&amp;cPath=65&amp;limit=99" class="">99</a>
+                    </li>
+                </ul>
+                <div class="page-links r"> &nbsp;<strong class="current item">1</strong>&nbsp;&nbsp;<a class="item" title=" Page 2 " href="http://localhost/zc_mood/index.php?main_page=index&amp;cPath=65&amp;page=2">2</a>&nbsp;&nbsp;<a class="next item" title=" Next Page " href="http://localhost/zc_mood/index.php?main_page=index&amp;cPath=65&amp;page=2">NEXT</a>&nbsp;</div>
+            </div>
+            <ul class="product-listing fix">
+                <li class="product-box l rel"><div class="img-wrapper"><a href="http://localhost/zc_mood/index.php?main_page=product_info&amp;cPath=65_70_74&amp;products_id=180"><img width="300" height="300" title="Yellow Checkered Cotton Canvas" alt="Yellow Checkered Cotton Canvas" src="images/large/304669.jpg"></a></div><h2 class="name"><a href="http://localhost/zc_mood/index.php?main_page=product_info&amp;cPath=65_70_74&amp;products_id=180">Yellow Checkered Cotton Canvas</a></h2><div class="model">Product #:0001-1</div><div class="price">Price: <span class="normal-price through">$17.99</span><span class="sale-price">$9.99</span></div><div data="180" class="cart-box abs f12 dn"><div class="mask abs"></div><div class="input-box rel">QTY:<input type="text" class="ml5" maxlength="4" value="1" name="qty"> Yards</div><button class="submit rel">ADD To Cart</button></div></li><li class="product-box l rel"><div class="img-wrapper"><a href="http://localhost/zc_mood/index.php?main_page=product_info&amp;cPath=65_70_74&amp;products_id=181"><img width="300" height="300" title="Off-White/Lime Floral Canvas" alt="Off-White/Lime Floral Canvas" src="images/large/HP8166.jpg"></a></div><h2 class="name"><a href="http://localhost/zc_mood/index.php?main_page=product_info&amp;cPath=65_70_74&amp;products_id=181">Off-White/Lime Floral Canvas</a></h2><div class="model">Product #:0001-2</div><div class="price">Price: <span class="normal-price no-through">$19.99</span></div><div data="181" class="cart-box abs f12 dn"><div class="mask abs"></div><div class="input-box rel">QTY:<input type="text" class="ml5" maxlength="4" value="1" name="qty"> Yards</div><button class="submit rel">ADD To Cart</button></div></li><li class="product-box l rel"><div class="img-wrapper"><a href="http://localhost/zc_mood/index.php?main_page=product_info&amp;cPath=65_70_74&amp;products_id=182"><img width="300" height="300" title="Violet Solid Backed Canvas" alt="Violet Solid Backed Canvas" src="images/large/HP21557.jpg"></a></div><h2 class="name"><a href="http://localhost/zc_mood/index.php?main_page=product_info&amp;cPath=65_70_74&amp;products_id=182">Violet Solid Backed Canvas</a></h2><div class="model">Product #:0001-3</div><div class="price">Price: <span class="normal-price no-through">$19.99</span></div><div data="182" class="cart-box abs f12 dn"><div class="mask abs"></div><div class="input-box rel">QTY:<input type="text" class="ml5" maxlength="4" value="1" name="qty"> Yards</div><button class="submit rel">ADD To Cart</button></div></li><li class="product-box l rel"><div class="img-wrapper"><a href="http://localhost/zc_mood/index.php?main_page=product_info&amp;cPath=65_70_74&amp;products_id=183"><img width="300" height="300" title="Cream and Black Buffalo Check Cotton" alt="Cream and Black Buffalo Check Cotton" src="images/large/103649.jpg"></a></div><h2 class="name"><a href="http://localhost/zc_mood/index.php?main_page=product_info&amp;cPath=65_70_74&amp;products_id=183">Cream and Black Buffalo Check Cotton</a></h2><div class="model">Product #:0001-4</div><div class="price">Price: <span class="normal-price no-through">$24.99</span></div><div data="183" class="cart-box abs f12 dn"><div class="mask abs"></div><div class="input-box rel">QTY:<input type="text" class="ml5" maxlength="4" value="1" name="qty"> Yards</div><button class="submit rel">ADD To Cart</button></div></li><li class="product-box l rel"><div class="img-wrapper"><a href="http://localhost/zc_mood/index.php?main_page=product_info&amp;cPath=65_70_74&amp;products_id=184"><img width="300" height="300" title="Apricot Ice Midweight Water-Resistant Cotton Canvas" alt="Apricot Ice Midweight Water-Resistant Cotton Canvas" src="images/large/304289.jpg"></a></div><h2 class="name"><a href="http://localhost/zc_mood/index.php?main_page=product_info&amp;cPath=65_70_74&amp;products_id=184">Apricot Ice Midweight Water-Resistant Cotton Canvas</a></h2><div class="model">Product #:0005</div><div class="price">Price: <span class="normal-price through">$89.99</span><span class="sale-price">$29.99</span></div><div data="184" class="cart-box abs f12 dn"><div class="mask abs"></div><div class="input-box rel">QTY:<input type="text" class="ml5" maxlength="4" value="1" name="qty"> Yards</div><button class="submit rel">ADD To Cart</button></div></li><li class="product-box l rel"><div class="img-wrapper"><a href="http://localhost/zc_mood/index.php?main_page=product_info&amp;cPath=65_70_74&amp;products_id=185"><img width="300" height="300" title="Bright Green Midweight Water-Resistant Cotton Canvas" alt="Bright Green Midweight Water-Resistant Cotton Canvas" src="images/large/304290.jpg"></a></div><h2 class="name"><a href="http://localhost/zc_mood/index.php?main_page=product_info&amp;cPath=65_70_74&amp;products_id=185">Bright Green Midweight Water-Resistant Cotton Canvas</a></h2><div class="model">Product #:0006</div><div class="price">Price: <span class="normal-price no-through">$9.99</span></div><div data="185" class="cart-box abs f12 dn"><div class="mask abs"></div><div class="input-box rel">QTY:<input type="text" class="ml5" maxlength="4" value="1" name="qty"> Yards</div><button class="submit rel">ADD To Cart</button></div></li><li class="product-box l rel"><div class="img-wrapper"><a href="http://localhost/zc_mood/index.php?main_page=product_info&amp;cPath=65_70_74&amp;products_id=186"><img width="300" height="300" title="Return Policy | Shipping Info Deco Rose Herringbone Water-Resis" alt="Return Policy | Shipping Info Deco Rose Herringbone Water-Resis" src="images/large/304291.jpg"></a></div><h2 class="name"><a href="http://localhost/zc_mood/index.php?main_page=product_info&amp;cPath=65_70_74&amp;products_id=186">Return Policy | Shipping Info Deco Rose Herringbone Water-Resis</a></h2><div class="model">Product #:0007</div><div class="price">Price: <span class="normal-price no-through">$24.99</span></div><div data="186" class="cart-box abs f12 dn"><div class="mask abs"></div><div class="input-box rel">QTY:<input type="text" class="ml5" maxlength="4" value="1" name="qty"> Yards</div><button class="submit rel">ADD To Cart</button></div></li><li class="product-box l rel"><div class="img-wrapper"><a href="http://localhost/zc_mood/index.php?main_page=product_info&amp;cPath=65_70_74&amp;products_id=187"><img width="300" height="300" title="Return Policy | Shipping Info Almond Buff Midweight Water-Resis" alt="Return Policy | Shipping Info Almond Buff Midweight Water-Resis" src="images/large/304292.jpg"></a></div><h2 class="name"><a href="http://localhost/zc_mood/index.php?main_page=product_info&amp;cPath=65_70_74&amp;products_id=187">Return Policy | Shipping Info Almond Buff Midweight Water-Resis</a></h2><div class="model">Product #:</div><div class="price">Price: <span class="normal-price no-through">$89.99</span></div><div data="187" class="cart-box abs f12 dn"><div class="mask abs"></div><div class="input-box rel">QTY:<input type="text" class="ml5" maxlength="4" value="1" name="qty"> Yards</div><button class="submit rel">ADD To Cart</button></div></li><li class="product-box l rel"><div class="img-wrapper"><a href="http://localhost/zc_mood/index.php?main_page=product_info&amp;cPath=65_70_74&amp;products_id=188"><img width="300" height="300" title="White Midweight Water-Resistant Cotton Canvas" alt="White Midweight Water-Resistant Cotton Canvas" src="images/large/304294.jpg"></a></div><h2 class="name"><a href="http://localhost/zc_mood/index.php?main_page=product_info&amp;cPath=65_70_74&amp;products_id=188">White Midweight Water-Resistant Cotton Canvas</a></h2><div class="model">Product #:0045</div><div class="price">Price: <span class="normal-price no-through">$59.99</span></div><div data="188" class="cart-box abs f12 dn"><div class="mask abs"></div><div class="input-box rel">QTY:<input type="text" class="ml5" maxlength="4" value="1" name="qty"> Yards</div><button class="submit rel">ADD To Cart</button></div></li><li class="product-box l rel"><div class="img-wrapper"><a href="http://localhost/zc_mood/index.php?main_page=product_info&amp;cPath=65_70_74&amp;products_id=189"><img width="300" height="300" title="Harbor Blue Midweight Water-Resistant Cotton Canvas" alt="Harbor Blue Midweight Water-Resistant Cotton Canvas" src="images/304295.jpg"></a></div><h2 class="name"><a href="http://localhost/zc_mood/index.php?main_page=product_info&amp;cPath=65_70_74&amp;products_id=189">Harbor Blue Midweight Water-Resistant Cotton Canvas</a></h2><div class="model">Product #:0049</div><div class="price">Price: <span class="normal-price no-through">$198.99</span></div><div data="189" class="cart-box abs f12 dn"><div class="mask abs"></div><div class="input-box rel">QTY:<input type="text" class="ml5" maxlength="4" value="1" name="qty"> Yards</div><button class="submit rel">ADD To Cart</button></div></li></ul>
+            <script>
+                $(function(){
+                    $('.product-box').hover(function(){
+                        $(this).find('.cart-box').removeClass('dn');
+                    },function(){
+                        $(this).find('.cart-box').addClass('dn');
+                    });
+                    $('.cart-box button').click(function(e){
+                        var num = $(this).parent().find('input[name="qty"]').val();
+                        var productId  = $(this).parent().attr('data');
+                        if(!/^\d+$/.test(num)){
+                            alert('Please input a number');
+                            return false;
+                        }
+                        var $dot = $('.animated-dot');
+                        var $end = $('#header-wrapper .header-cart-status');
+                        var offsetLeft = $end[0].getBoundingClientRect().left, offsetTop = $(window).scrollTop();
+                        $dot.css({'left':e.pageX,'top':e.pageY}).removeClass('dn');
+
+                        $dot.animate({'left':offsetLeft,'top':offsetTop},'slow',function(){
+                            $dot.addClass('dn');
+                            $('.header-cart-box').html('&lt;div class="loading"&gt;&lt;/div&gt;').removeClass('dn');
+                            var url = 'index.php?action=add_product&amp;in_ajax=1';
+                            var data =
+                            {
+                                'products_id':productId,
+                                'cart_quantity':1,
+                                'securityToken':'78dd7363e1c0e2178bae26e6c59237f9'
+                            }
+                            var callback = function(response){
+                                if(response.result == 'success'){
+                                    Mobanbase.getCart();
+                                }
+                            };
+                            $.post(url, data, callback, 'JSON');
+                        });
+                    });
+                });
+            </script>
+            <div class="split-page fix">
+                <ul class="limit fix l">
+                    <li style="padding-right: 5px" class="l">Items Per Page:</li>
+                    <li class="l">
+                        <a href="http://localhost/zc_mood/index.php?main_page=index&amp;cPath=65&amp;limit=27" class="">27</a>
+                    </li>
+                    <li class="l" style="padding: 0 5px">|</li>
+                    <li class="l">
+                        <a href="http://localhost/zc_mood/index.php?main_page=index&amp;cPath=65&amp;limit=54" class="">54</a>
+                    </li>
+                    <li class="l" style="padding: 0 5px">|</li>
+                    <li class="l">
+                        <a href="http://localhost/zc_mood/index.php?main_page=index&amp;cPath=65&amp;limit=99" class="">99</a>
+                    </li>
+                </ul>
+                <div class="page-links r"> &nbsp;<strong class="current item">1</strong>&nbsp;&nbsp;<a class="item" title=" Page 2 " href="http://localhost/zc_mood/index.php?main_page=index&amp;cPath=65&amp;page=2">2</a>&nbsp;&nbsp;<a class="next item" title=" Next Page " href="http://localhost/zc_mood/index.php?main_page=index&amp;cPath=65&amp;page=2">NEXT</a>&nbsp;</div>
+            </div>
+            <div class="abs animated-dot dn">dot</div>
+        </div>
+
+    </div>
+
+</div>
